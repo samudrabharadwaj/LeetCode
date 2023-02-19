0d84ac27ae47396d6e27a1fb9333f9777d154142
samudrabharadwaj <122354552+samudrabharadwaj@users.noreply.github.com>
2023-02-20 00:49:30 +0530

    Time: 66 ms (53.51%), Space: 41.7 MB (85.96%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    if(s.length==0) return 0;
+    
+    let array = s.trim().split(' ');
+    return array[array.length-1].length  ;
+    
+};
